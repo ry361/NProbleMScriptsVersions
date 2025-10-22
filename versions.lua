217226db68cbdd0ffebd577b0e273486a815b529
ry <75577692+ry361@users.noreply.github.com>
2025-10-23 01:23:36 +0300

    Update versions.lua

--- versions.lua
+++ versions.lua
@@ -2,7 +2,7 @@ return {
     stashScript = "1.0.5",
     taxiCompany = "1.0.6",
     newspaperv3 = "1.0.3",
-    boxingv2 = "1.0.0",
+    boxingv2 = "1.0.1",
     drivingSchool = "1.0.3",
     dealerShip = "1.0.9",
     radar = "1.0.0"
